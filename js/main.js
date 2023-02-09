876faedb0b9e436d252d5f7dd7fa12a5558722de
clariza2002 <claritzcastillo@gmail.com>
2023-02-09 11:13:12 -0500

    Agregando carpetas

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.write(
+'<nav class="navbar bg-primary navbar-expand-lg  "  data-bs-theme="dark">'+
+'<div class="container-fluid">'+
+'<a class="navbar-brand" href="#">Chismesito y trabajo</a>'+
+'<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">'+
+'<span class="navbar-toggler-icon"></span>'+
+'</button>'+
+'<div class="collapse navbar-collapse" id="navbarScroll">'+
+'<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">'+
+'<li class="nav-item">'+
+'<a class="nav-link active" aria-current="page" href="#">Home</a>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link" href="/b_ej/html/integrantes.html">Integrantes</a>'+
+'</li>'+
+'<li class="nav-item dropdown">'+
+'<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
+'Lenguajes'+
+'</a>'+
+'<ul class="dropdown-menu">'+
+'<li><a class="dropdown-item" href="/b_ej/html/java.html">Java</a></li>'+
+'<li><a class="dropdown-item" href="/b_ej/html/c.html">C</a></li>'+
+'<li><a class="dropdown-item" href="/b_ej/html/python.html">Python</a></li>'+
+'<li><hr class="dropdown-divider"></li>'+
+'<li><a class="dropdown-item" href="#">Otros</a></li>'+
+'</ul>'+
+'</li>'+
+'<li class="nav-item">'+
+'<a class="nav-link " href="/b_ej/html/contactos.html">Contacto</a>'+
+'</li>'+
+'</ul>'+
+'<form class="d-flex" role="search">'+
+'<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">'+
+'<button class="btn btn-outline-success" type="submit">Search</button>'+
+'</form>'+
+'</div>'+
+'</div>'+
+'</nav>'
+);
---
+++ js/menu3.js
@@ -0,0 +1,58 @@
+document.write(
+
+
+    '<nav class="navbar bg-body-tertiary fixed-top">'+
+    '    <div class="container-fluid">'+
+    '    '+
+    '      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">'+
+    '        <span class="navbar-toggler-icon"></span>'+
+    '      </button>'+
+    '      <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">'+
+    '        <div class="offcanvas-header">'+
+    '          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menú de opciones</h5>'+
+    '          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>'+
+    '        </div>'+
+    '        <div class="offcanvas-body">'+
+    '          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">'+
+    '            <li class="nav-item">'+
+    '             '+
+    '                    <a class="nav-link active" aria-current="page" href="Reserva_cliente.html">  <img src="../assets/img/iconReserva.png" alt="Clientes" width="30" height="24">  Reserva</a>'+
+    '                 '+
+    '            </li>'+
+''+
+    '            <li class="nav-item">'+
+    '              '+
+    '              <a class="nav-link active" href="Informacion_Cliente.html"> <img src="../assets/img/iconUser.png" alt="Clientes" width="30" height="24">  Información personal</a>'+
+    '                  '+
+    '            </li>'+
+''+
+    '            <li class="nav-item">'+
+    '              '+
+    '              <a class="nav-link active" href="eliminarCuentaCliente.html"> <img src="../assets/img/iconDelete.png" alt="Clientes" width="30" height="24">Eliminar cuenta</a>'+
+    '                  '+
+    '            </li>'+
+    '            '+
+    '              <li class="nav-item">'+
+    '                '+
+    '                <a class="nav-link active" href="cerrarSesionCliente.html"> <img src="../assets/img/iconCerrar.png" alt="Clientes" width="30" height="24">Cerrar sesión</a>'+
+    '               '+
+    '              </li>'+
+    '            '+
+    '          </ul>'+
+    '          '+
+    '        </div>'+
+    '      </div>'+
+    '      <table id="table-spacing">'+
+    '        <tr>'+
+    '          <td class="bg-green round-corner-top">'+
+    '            <img src="../assets/img/icon1.png" alt="img-icon1" style="padding: -50px; ">'+
+    '          </td>'+
+    '          <td class="bg-orange round-corner-top">'+
+    '            <a class="navbar-brand" href="#">HOTEL MAR AZUL</a>'+
+    '          </td>'+
+    '        </tr> '+
+    '      </table>'+
+    '    </div>'+
+    '    '+
+    '  </nav>'
+)
