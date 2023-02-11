92d9680c8f6c7325ffe981ab70e2a5d9db872003
yesecastro06@udenar.edu.co <yeca1007>
2023-02-10 21:09:09 -0500

    agragando conexion

---
+++ Reg_servicios.php
@@ -0,0 +1,21 @@
+<?php
+	include 'conexion.php';
+	session_start();	
+	$nombre= $_POST['NomServicio'];
+	$precio =$_POST['CostServicio'];
+	
+	$sql = "INSERT INTO servicios values( '','$nombre', '$precio')";
+		$_consulta = mysqli_query($conexion, $sql);
+		if($_consulta){			
+			echo '
+			<script> 
+				alert("se registro exitosamente");
+
+			</script>
+		';	
+        }
+        else{
+            echo 'error de consulta';
+        }
+
+?>
---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$conexion=mysqli_connect("localhost","root","","hotelmarazul");
+
+if (!$conexion) {
+    echo "Error: No se pudo conectar a la base de datos." ;
+    
+}
+
+echo "Éxito";
+
+?>
