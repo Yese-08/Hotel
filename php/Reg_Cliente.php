e00f5f2e9e73b2854ca8ceaf27eedc21a03d1510
clariza2002 <claritzcastillo@gmail.com>
2023-02-10 23:35:23 -0500

    agregando reg_cliente.php

--- php/Reg_Cliente.php
+++ php/Reg_Cliente.php
@@ -1,11 +1,18 @@
 <?php
 	include 'conexion.php';
 	session_start();	
-	$nombre= $_POST['NomServicio'];
-	$precio =$_POST['CostServicio'];
-    
+	$NomApe= $_POST['NomApe'];
+	$TipoDoc =$_POST['TipoDoc'];
+	$NDocum =$_POST['NDocum'];
+    $fechaIngreso =$_POST['fechaIngreso'];
+	$Nacionalidad =$_POST['Nacionalidad'];
+	$numPer =$_POST['numPer'];
+	$Email =$_POST['Email'];
+	$Genero =$_POST['Genero'];
+	$Usuario =$_POST['Usuario'];
+	$contraseña =$_POST['contraseña'];
 	
-	$sql = "INSERT INTO servicios values( '','$nombre', '$precio')";
+	$sql = "INSERT INTO cliente values('$NomApe', '$TipoDoc','$NDocum', '$fechaIngreso', '$Nacionalidad', '$numPer', '$Email', '$Genero', '$Usuario', '$contraseña')";
 		$_consulta = mysqli_query($conexion, $sql);
 		if($_consulta){			
 			echo '
