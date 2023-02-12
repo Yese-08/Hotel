8ecbe2ca7851bf554f6cd787a5248120ca6cdd99
harold2224 <harold.avila24@gmail.com>
2023-02-11 19:01:05 -0500

    Agregando Reg_Reserva.php

--- php/Reg_reserva.php
+++ php/Reg_reserva.php
@@ -11,7 +11,7 @@ $email =$_POST['Email'];
 $genero =$_POST['genero'];
 $numero_hab=$_POST['Nhabitacion'];
 $costo =$_POST['CostoH'];
-$numero_personas =$_POST['NPerso'];
+$numero_personas =$_POST['Nperso'];
 $fecha_ingreso =$_POST['FIngreso'];
 $fecha_salida =$_POST['FSalida'];
 $numero_noches =$_POST['NNoches'];
@@ -20,18 +20,21 @@ $costo_servicio =$_POST['CServicio'];
 $total_pagar =$_POST['TPagar'];
 
 
+
 $sql = "INSERT INTO reserva values('', '$nombre_client','$tipo_documento', '$numero_documento', '$fecha_nacimiento', '$nacionalidad','$telefono', '$email', '$genero', '$numero_hab', '$costo','$numero_personas','$fecha_ingreso','$fecha_salida','$numero_noches','$servicios_adicionales','$costo_servicio','$total_pagar')";
     $_consulta = mysqli_query($conexion, $sql);
     if($_consulta ){		
         echo '
 			<script> 
 				alert("se registro exitosamente");
-
 			</script>
 		';	
     }
     else{
         echo 'error de consulta';
     }
+
+
+
 
 ?>
