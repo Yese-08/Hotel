aafb992f99fb4330fb7088f997d0fe240c5d6b45
harold2224 <harold.avila24@gmail.com>
2023-02-19 21:11:21 -0500

    Modific index

--- index.php
+++ index.php
@@ -362,7 +362,7 @@ Swal.fire({
     //Read more about isConfirmed, isDenied below 
     if (result.isConfirmed) {
       
-      window.location.href = "http://localhost/conGra/proyecto/html/Login.html";
+      window.location.href = "http://localhost/proyecto/html/Login.html";
     } else if (result.isDenied) {
       Swal.fire('Changes are not saved', '', 'info')
     }
